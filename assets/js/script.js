5edfd5dc78f2e3ab0dc453e85fe80a0b5ece3234
mahdibinshahadat <bestideas00>
2024-07-26 05:17:09 -0600

    1st commit

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+$('.text_slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow:'<i class="fa-solid fa-angle-left prev"></i>',
+    nextArrow:'<i class="fa-solid fa-angle-right next"></i>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
