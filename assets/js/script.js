5348daf83a6ea6ab3849e5cebbceb55d75539e6a
mahdibinshahadat <bestideas00>
2024-08-19 02:20:26 +0600

    full design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+
+// vhano_box
+new VenoBox({
+    selector: '.my-image-links',
+    numeration: true,
+    infinigall: true,
+    share: true,
+    spinner: 'rotating-plane'
+});
+
+// counter_start
 $('.counter').counterUp({
     delay: 10,
     time: 1000
